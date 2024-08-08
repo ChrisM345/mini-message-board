62836239c8c3bb316fe094219b4868938aff739e
ChrisM345 <chrism.12277@gmail.com>
2024-08-07 20:32:07 -0500

    Updated main app.js

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-  },
-];
-
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -21,6 +8,7 @@ app.set("view engine", "ejs");
 
 const indexRoute = require("./routes/index");
 const messageRoute = require("./routes/message");
+app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRoute);
 app.use("/", messageRoute);
 
